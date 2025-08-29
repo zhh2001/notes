6f3a4af18bf72c8f77ffff3ce874d893beeab86e
Zhang Henghua <1652709417@qq.com>
2025-08-29 11:37:44 +0800

    update notes

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,74 +1,27 @@
 import { defineConfig } from 'vitepress'
 
 const SDNNoteItems = [
+  { text: 'P4 Language', link: '/sdn/p4' },
+  { text: 'P4 Exercise', link: '/sdn/p4_exercise' },
+  { text: 'P4 INT', link: '/sdn/int' },
+  { text: 'P4 Utils', link: '/sdn/p4_utils' },
+  { text: 'Mininet', link: '/sdn/mininet' },
+  { text: 'iPerf', link: '/sdn/iperf' },
   {
-    text: 'P4 Language',
-    link: '/sdn/p4'
+    text: 'Writing', link: '/sdn/writing/', items: [
+      { text: 'Formula', link: '/sdn/writing/formula' },
+      { text: 'Algorithm', link: '/sdn/writing/algorithm' },
+      { text: 'Table', link: '/sdn/writing/table' },
+      { text: 'Bibliography', link: '/sdn/writing/bibliography' }
+    ]
   },
-  {
-    text: 'P4 Exercise',
-    link: '/sdn/p4_exercise'
-  },
-  {
-    text: 'P4 INT',
-    link: '/sdn/int'
-  },
-  {
-    text: 'P4 Utils',
-    link: '/sdn/p4_utils'
-  },
-  {
-    text: 'Mininet',
-    link: '/sdn/mininet'
-  },
-  {
-    text: 'iPerf',
-    link: '/sdn/iperf'
-  },
-  {
-    text: 'Writing',
-    link: '/sdn/writing/',
-    items: [
-      {
-        text: 'Formula',
-        link: '/sdn/writing/formula',
-      },
-      {
-        text: 'Algorithm',
-        link: '/sdn/writing/algorithm',
-      },
-      {
-        text: 'Table',
-        link: '/sdn/writing/table',
-      },
-      {
-        text: 'Bibliography',
-        link: '/sdn/writing/bibliography',
-      },
-    ],
-  },
-  {
-    text: 'CCF',
-    link: '/sdn/ccf'
-  },
+  { text: 'CCF', link: '/sdn/ccf' }
 ]
 const GoNoteItems = [
-  {
-    text: 'GoLang',
-    link: '/go/golang'
-  },
-  {
-    text: 'Goroutine',
-    link: '/go/goroutine'
-  },
-  {
-    text: 'Gin',
-    link: '/go/gin'
-  },
-  {
-    text: 'gRPC',
-    link: '/go/grpc'
-  },
+  { text: 'GoLang', link: '/go/golang' },
+  { text: 'Goroutine', link: '/go/goroutine' },
+  { text: 'Gin', link: '/go/gin' },
+  { text: 'gRPC', link: '/go/grpc' }
 ]
 
 const keywords = [
@@ -87,8 +40,6 @@ const keywords = [
   'Go协程',
   'Go开发',
   'SDN控制器',
-  'SDN仿真实验',
-  'SDN网络监控',
   'Henghua Zhang',
   'Software Defined Network',
   'Software-Defined Networking',
@@ -97,7 +48,6 @@ const keywords = [
   'PDP',
   'Programming Protocol-independent Packet Processors',
   'P4',
-  'p4app',
   'P4 Lang',
   'P4 Language',
   'P4Runtime',
@@ -131,61 +81,14 @@ export default defineConfig({
   cleanUrls: true,
   ignoreDeadLinks: true,
   head: [
-    [
-      'meta',
-      {
-        name: 'keywords',
-        content: keywords.join(', '),
-      },
-    ],
-    [
-      'meta',
-      {
-        name: 'google-site-verification',
-        content: 'wMOTcBwCiCMV7ESftQRY3Glvq8UL4xzUKrZ-1wjOpqM',
-      },
-    ],
-    [
-      'link',
-      {
-        rel: 'icon',
-        href: '/sues.png',
-      },
-    ],
-    [
-      'link',
-      {
-        rel: 'preconnect',
-        href: 'https://fonts.googleapis.com',
-      },
-    ],
-    [
-      'link',
-      {
-        rel: 'preconnect',
-        href: 'https://fonts.gstatic.com',
-        crossorigin: '',
-      },
-    ],
-    [
-      'link',
-      {
-        href: 'https://fonts.googleapis.com/css2?family=Noto+Serif+SC:wght@200..900&display=swap',
-        rel: 'stylesheet',
-      },
-    ],
-    [
-      'script',
-      {
-        async: '',
-        src: 'https://www.googletagmanager.com/gtag/js?id=G-414XCQ3MDV',
-      },
-    ],
-    [
-      'script',
-      {
-
-      },
+    ['meta', { name: 'keywords', content: keywords.join(', ') }],
+    ['meta', { name: 'google-site-verification', content: 'wMOTcBwCiCMV7ESftQRY3Glvq8UL4xzUKrZ-1wjOpqM' }],
+    ['link', { rel: 'icon', href: '/sues.png' }],
+    ['link', { rel: 'preconnect', href: 'https://fonts.googleapis.com' }],
+    ['link', { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: '' }],
+    ['link', { href: 'https://fonts.googleapis.com/css2?family=Noto+Serif+SC:wght@200..900&display=swap', rel: 'stylesheet' }],
+    ['script', { async: '', src: 'https://www.googletagmanager.com/gtag/js?id=G-414XCQ3MDV' }],
+    ['script', {},
       `
       window.dataLayer = window.dataLayer || [];
       function gtag() {
@@ -193,29 +96,22 @@ export default defineConfig({
       }
       gtag('js', new Date());
       gtag('config', 'G-414XCQ3MDV');
-      `,
-    ],
+      `
+    ]
   ],
-  i18nRouting: false,
-  locales: {
-    root: {
-      label: '中文',
-      lang: 'zh-Hans',
-    },
-  },
   markdown: {
     container: {
       dangerLabel: '危险',
       detailsLabel: '详细信息',
       infoLabel: '信息',
       tipLabel: '提示',
-      warningLabel: '警告',
+      warningLabel: '警告'
     },
     math: true,
     languageAlias: {
       'p4': 'c++',
-      'pseudo': 'c++',
-    },
+      'pseudo': 'c++'
+    }
   },
   themeConfig: {
     siteTitle: '张恒华',
@@ -225,50 +121,31 @@ export default defineConfig({
     darkModeSwitchTitle: '切换到深色模式',
     sidebarMenuLabel: '菜单',
     returnToTopLabel: '回到顶部',
-    docFooter: {
-      prev: '上一页',
-      next: '下一页',
-    },
+    docFooter: { prev: '上一页', next: '下一页' },
     editLink: {
-      pattern: ({ filePath }) => {
-        return `https://github.com/zhh2001/notes/tree/main/docs/${filePath}`
-      },
-      text: '在 GitHub 上编辑此页面',
+      pattern: ({ filePath }) => `https://github.com/zhh2001/notes/tree/main/docs/${filePath}`,
+      text: '在 GitHub 上编辑此页面'
     },
     footer: {
       message: '基于 <a href="/mit">MIT 许可</a> 发布',
-      copyright: '版权所有 © 2024至今 <a href="/resume">张恒华</a>',
+      copyright: '版权所有 © 2024至今 <a href="/resume">张恒华</a>'
     },
     nav: [
-      {
-        text: '首页',
-        link: '/',
-      },
+      { text: '首页', link: '/' },
       {
         text: '学习笔记',
         items: [
-          {
-            text: 'SDN',
-            items: SDNNoteItems,
-          },
-          {
-            text: 'Go',
-            items: GoNoteItems,
-          },
+          { text: 'SDN', items: SDNNoteItems },
+          { text: 'Go', items: GoNoteItems }
         ],
       },
-      {
-        text: '关于我',
-        link: '/resume',
-      },
+      { text: '关于我', link: '/resume' }
     ],
     search: {
       provider: 'local',
       options: {
         translations: {
-          button: {
-            buttonText: '搜索文档',
-          },
+          button: { buttonText: '搜索文档' },
           modal: {
             displayDetails: '显示详细内容',
             resetButtonTitle: '清除查询条件',
@@ -276,72 +153,28 @@ export default defineConfig({
             footer: {
               selectText: '选择',
               navigateText: '切换',
-              closeText: '关闭',
-            },
-          },
-        },
-      },
+              closeText: '关闭'
+            }
+          }
+        }
+      }
     },
     sidebar: [
-      {
-        text: 'SDN',
-        link: '/sdn/',
-        items: SDNNoteItems,
-      },
-      {
-        text: 'Go',
-        items: GoNoteItems,
-      }
+      { text: 'SDN', link: '/sdn/', items: SDNNoteItems },
+      { text: 'Go', items: GoNoteItems }
     ],
     notFound: {
       code: '404',
       linkText: '返回首页',
       quote: '请检查页面路径是否正确',
-      title: '页面未找到',
+      title: '页面未找到'
     },
     socialLinks: [
-      {
-        icon: 'csdn',
-        link: 'https://blog.csdn.net/qq_43133192',
-      },
-      {
-        icon: 'github',
-        link: 'https://github.com/zhh2001',
-      },
-      {
-        icon: 'orcid',
-        link: 'https://orcid.org/0009-0005-9456-8936',
-      },
-      {
-        icon: 'qq',
-        link: 'mailto:1652709417@qq.com',
-      },
-    ],
+      { icon: 'csdn', link: 'https://blog.csdn.net/qq_43133192' },
+      { icon: 'github', link: 'https://github.com/zhh2001' },
+      { icon: 'orcid', link: 'https://orcid.org/0009-0005-9456-8936' },
+      { icon: 'qq', link: 'mailto:1652709417@qq.com' }
+    ]
   },
-  sitemap: {
-    hostname: 'https://zhh2001.github.io',
-    transformItems: items => {
-      console.log('Transform Items')
-      console.log(items)
-      return items
-    }
-  },
-
-  // 构建钩子
-  async buildEnd(siteConfig) {
-    console.log('Build End')
-    console.log(siteConfig)
-  },
-  async postRender(context) {
-    console.log('Post Render')
-    console.log(context)
-  },
-  async transformHead(context) {
-    console.log('Transform Head')
-    console.log(context)
-  },
-  async transformHtml(code, id, context) {
-    console.log('Transform HTML')
-    console.log(code, id, context)
-  }
+  sitemap: { hostname: 'https://zhh2001.github.io' }
 })
